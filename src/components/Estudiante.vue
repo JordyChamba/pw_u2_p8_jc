95154cb3f855b606be638e427bbf130a73c68827
JordyChamba <jordy_chamba@hotmail.com>
2025-05-15 22:17:38 -0500

    AgregarTresAtributos

--- src/components/Estudiante.vue
+++ src/components/Estudiante.vue
@@ -2,18 +2,33 @@
   <div class="container">
     <label for="id_nombre">Nombre: </label>
     <input v-model="nuevoNombre" id="id_nombre" type="text" />
+
     <label for="id_apellido">Apellido: </label>
     <input v-model="nuevoApellido" id="id_apellido" type="text" />
-    <!--- Elemento de entrada -->
+
+    <label for="id_edad">Edad: </label>
+    <input v-model="nuevaEdad" id="id_edad" type="number" />
+
+    <label for="id_carrera">Carrera: </label>
+    <input v-model="nuevaCarrera" id="id_carrera" type="text" />
+
+    <label for="id_ciudad">Ciudad: </label>
+    <input v-model="nuevaCiudad" id="id_ciudad" type="text" />
+
     <button v-on:click="agregarEstudiante">Agregar Estudiante</button>
-    {{ nuevoNombre}}<!--- Se vinculo, los dos se emparejan -->
+
     <ul>
-      <li v-for="{ nombre, apellido } in lista" :Key="nombre">
-        Nombre: {{ nombre }} - Apellido: {{ apellido }}
+      <li
+        v-for="{ nombre, apellido, edad, carrera, ciudad } in lista"
+        :key="nombre + apellido"
+      >
+        Nombre: {{ nombre }} - Apellido: {{ apellido }} - Edad: {{ edad }} -
+        Carrera: {{ carrera }} - Ciudad: {{ ciudad }}
       </li>
     </ul>
   </div>
 </template>
+
 <!--- 
  - v-for: iteracion 
  - comunicar datos de entrada con una propiedad ractiva
@@ -26,27 +41,81 @@
 export default {
   data() {
     return {
-      nuevoNombre: "nuevo",
-      nuevoApellido: "nuevo",
+      nuevoNombre: "",
+      nuevoApellido: "",
+      nuevaEdad: null,
+      nuevaCarrera: "",
+      nuevaCiudad: "",
       lista: [
-        { nombre: "Jordy", apellido: "Chamba" },
-        { nombre: "Kevin", apellido: "Teran" },
-        { nombre: "Jose", apellido: "Castillo" },
-        { nombre: "Ana", apellido: "Pozo" },
-        { nombre: "Erik", apellido: "Parra" },
-        { nombre: "Pedro", apellido: "Loya" },
-        { nombre: "Juan", apellido: "Andrade" },
+        {
+          nombre: "Jordy",
+          apellido: "Chamba",
+          edad: 22,
+          carrera: "Sistemas",
+          ciudad: "Quito",
+        },
+        {
+          nombre: "Kevin",
+          apellido: "Teran",
+          edad: 23,
+          carrera: "Electrónica",
+          ciudad: "Guayaquil",
+        },
+        {
+          nombre: "Jose",
+          apellido: "Castillo",
+          edad: 21,
+          carrera: "Contabilidad",
+          ciudad: "Cuenca",
+        },
+        {
+          nombre: "Ana",
+          apellido: "Pozo",
+          edad: 20,
+          carrera: "Derecho",
+          ciudad: "Loja",
+        },
+        {
+          nombre: "Erik",
+          apellido: "Parra",
+          edad: 22,
+          carrera: "Medicina",
+          ciudad: "Ambato",
+        },
+        {
+          nombre: "Pedro",
+          apellido: "Loya",
+          edad: 24,
+          carrera: "Mecánica",
+          ciudad: "Riobamba",
+        },
+        {
+          nombre: "Juan",
+          apellido: "Andrade",
+          edad: 25,
+          carrera: "Arquitectura",
+          ciudad: "Ibarra",
+        },
       ],
     };
   },
-  methods:{
-    agregarEstudiante(){
-        const nuevo= {
-            nombre: this.nuevoNombre,
-            apellido: this.nuevoApellido ,
-        };
-        /*this.lista.unshift(nuevo);*/
-        this.lista.push(nuevo);
+  methods: {
+    agregarEstudiante() {
+      const nuevo = {
+        nombre: this.nuevoNombre,
+        apellido: this.nuevoApellido,
+        edad: this.nuevaEdad,
+        carrera: this.nuevaCarrera,
+        ciudad: this.nuevaCiudad,
+      };
+      this.lista.push(nuevo);
+
+      // Limpiar campos después de agregar
+      this.nuevoNombre = "";
+      this.nuevoApellido = "";
+      this.nuevaEdad = null;
+      this.nuevaCarrera = "";
+      this.nuevaCiudad = "";
     },
   },
 };
